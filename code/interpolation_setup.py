02158ff196c7a3f435d76c886fbf37adc7d51bfb
kothasaha715-afk <kothasaha715@gmail.com>
2025-09-24 11:38:58 +0600

    Implemented cubic spline evaluation" git push origin main

--- code/interpolation_setup.py
+++ code/interpolation_setup.py
@@ -3,6 +3,30 @@ from scipy.interpolate import lagrange
 from cubic_spline import cubic_spline_coeffs
 from spline_eval import cubic_spline_eval
 
+
+# --- Cubic spline evaluation function (moved here from spline_eval.py) ---
+def cubic_spline_eval(x, y, M, xi):
+    """
+    Evaluate the cubic spline at a point xi.
+    x, y : arrays of knots
+    M    : array of second derivatives at knots
+    xi   : point to evaluate
+    """
+    # Find the right interval for xi
+    i = np.searchsorted(x, xi) - 1
+    if i < 0:
+        i = 0
+    if i >= len(x) - 1:
+        i = len(x) - 2
+
+    h = x[i+1] - x[i]
+    A = (x[i+1] - xi) / h
+    B = (xi - x[i]) / h
+
+    # Cubic spline formula
+    Si = (A * y[i] + B * y[i+1] +
+          ((A**3 - A) * M[i] + (B**3 - B) * M[i+1]) * (h**2) / 6.0)
+    return Si
 # Sample temperature data
 x = np.array([0, 1, 2, 3, 4, 5, 6])
 y = np.array([15, 17, 20, 22, 21, 19, 16])
